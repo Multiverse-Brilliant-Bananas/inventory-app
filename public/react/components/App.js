03bf5ae81d2956578b0c8dc8b21cd68aa02bc353
Rose (Ariel) <ariel.bowen@aol.com>
2022-06-23 15:11:05 -0400

    added navbar

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -48,17 +48,35 @@ export const App = () => {
 	}, []);
 
 	return (
-		<main>
-			<div className='inventory-header'>
 
-      			<h1>Luxorzon</h1>
-				
-			</div>			
+
+		<main>
+					
+			<nav class="navbar navbar-expand-custom navbar-mainbg">
+        <a class="navbar-brand navbar-logo" href="#">Luxorzon</a>
+        <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars text-white"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
+                <li class="nav-item active">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>Address Book</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-clone"></i>Add Item</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
 
 				<ItemsList items={items} setitem={setitem} item={item} addItem={addItem}/>
 
 				<ItemForm items={items} setitem={setitem} item={item} addItem={addItem}/>
+			
 
+			
 		</main>
 	);
 
